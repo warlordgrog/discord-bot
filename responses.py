2f2a53a824edc2e54a29cf889a2ae0b4d125cfbd
warlordgrog <125464279+warlordgrog@users.noreply.github.com>
2023-02-15 14:20:07 +0200

    Update responses.py

--- responses.py
+++ responses.py
@@ -10,25 +10,7 @@ def get_response(message: str) -> str:
 
     if message == 'roll':
         return str(random.randint(1, 6))
-
-    if p_message.startswith('gejus'):
-        return 'taip'
-
-    if p_message.endswith('gejus'):
-        return 'taip'
-
-    if p_message.startswith('nig'):
-        return '🤨'
-
-    if p_message.endswith('nig'):
-        return '🤨'
-
-    if p_message.startswith('neg'):
-        return '🤨'
-
-    if p_message.endswith('neg'):
-        return '🤨'
-
+ 
     if message == 'Rock':
         return str(random.choice(a))
 
@@ -38,11 +20,6 @@ def get_response(message: str) -> str:
     if message == 'Scissors':
         return str(random.choice(a))
 
-    if p_message == 'dj khaled':
-        return 'https://tenor.com/view/dj-khaled-another-one-gif-24892740'
-
-    if p_message == 'cigonas':
-        return 'https://open.spotify.com/artist/7EJlB4S1doJPfMLfDGSp3P?si=ORFgZhEBQXqRXRutvDapQg'
 
 
 
